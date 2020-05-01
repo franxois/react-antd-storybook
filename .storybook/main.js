10bf9b1e3267250ee32d8400959a9bd6b9040eeb
franxois <franxoois@gmail.com>
2020-05-01 19:08:35 +0200

    fix(storybook): use antd LESS theme in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,54 @@ const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/preset-ant-design",
+    {
+      name: "@storybook/preset-create-react-app",
+      options: {
+        craOverrides: {
+          fileLoaderExcludes: ["less"],
+        },
+      },
+    },
+    // TODO : add when "@storybook/preset-ant-design" is fixed
+    // {
+    //   name: "@storybook/preset-ant-design", // TODO : remove when "@storybook/preset-ant-design" is fixed
+    //   options: {
+    //     lessOptions: {
+    //       modifyVars: {
+    //         "primary-color": "#f20f71",
+    //       },
+    //     },
+    //   },
+    // },
   ],
   webpackFinal: (config) => {
+    // TODO : remove when "@storybook/preset-ant-design" is fixed
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+              modifyVars: { "@primary-color": "#f20f71" },
+            },
+          },
+        },
+      ],
+    });
     config.plugins.push(new AntdDayjsWebpackPlugin());
+    return config;
+  },
+  babel: async (config, options) => {
+    // TODO : remove when "@storybook/preset-ant-design" is fixed
+    config.plugins.push([
+      "import",
+      { libraryName: "antd", libraryDirectory: "es", style: true },
+      "antd",
+    ]);
     return config;
   },
 };
--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -5,10 +5,10 @@ import "./App.css";
 
 export default { title: "Button" };
 
-export const withText = () => <Button>Hello Button</Button>;
+export const withText = () => <Button type="primary">Hello Button</Button>;
 
 export const withEmoji = () => (
-  <Button>
+  <Button type="primary">
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
